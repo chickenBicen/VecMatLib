1be23f02f492ed335cecee6e1cc86fca2275c2cf
chickenBicen <rds.jr24@gmail.com>
2026-03-26 01:25:58 -0500

    some utils

--- include/VecMatLib/utils.h
+++ include/VecMatLib/utils.h
@@ -1,6 +1,40 @@
 #pragma once
 #include "vec2.h"
+#include "vec3.h"
+#include "mat2.h"
+#include <cmath>
+#include <algorithm>
 
-namespace vecmatlib {
-	double magnitude( const vec2& v );
+namespace VecMatLib {
+    vec2 lerp( const vec2& a, const vec2& b, double t );
+    vec3 lerp( const vec3& a, const vec3& b, double t );
+
+    double magnitude( const vec2& v );
+
+    Mat2 rotationMat2( double angle );
+
+    template<typename Vec>
+        requires requires( Vec v )
+        {
+            v.dot(v);
+            v.magnitude();
+        }
+    double angle( const Vec& a, const Vec& b ) {
+        double dot = a.dot(b);
+        double mag = b.magnitude() * a.magnitude();
+
+        double val = dot / mag;
+        val = std::clamp(val, -1.0, 1.0);
+        return std::acos(val);
+    }
+
+    template<typename Vec>
+        requires requires( Vec v )
+        {
+            v.magnitude();
+        }
+    double distance( const Vec& a, const Vec& b ) {
+        Vec ab = a - b;
+        return ab.magnitude();
+    }
 } // namespace vecmatlib
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,5 +1,16 @@
 #include "../include/VecMatLib/utils.h"
 
-double vecmatlib::magnitude( const vec2& other ) {
-	return std::sqrt( other.getX() * other.getX() + other.getY() * other.getY() );
+#include <algorithm>
+#include <cmath>
+
+double VecMatLib::magnitude( const vec2& other ) {
+    return std::sqrt(other.x * other.x + other.y * other.y);
+}
+
+VecMatLib::Mat2 VecMatLib::rotationMat2( const double angle ) {
+    const double PI = 3.14159265358979323846;
+    return {
+        std::cos(angle), std::sin(angle),
+        -std::sin(angle), std::cos(angle)
+    };
 }
