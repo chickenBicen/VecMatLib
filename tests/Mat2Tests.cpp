7feb335268d74e67971a35e1dbd5b39b6a6cac39
chickenBicen <rds.jr24@gmail.com>
2026-03-29 15:35:07 -0500

    minor refactoring and made data publicly accessible

--- tests/Mat2Tests.cpp
+++ tests/Mat2Tests.cpp
@@ -4,51 +4,54 @@
 
 #include <cassert>
 #include <cmath>
+#include <iostream>
 
 #include "VecMatLib/Mat2.h"
 extern double EPS;
-extern void runTest( const char* name, void (*testFunc)() );
-extern bool nearlyEqual( const double a, const double b );
+extern void runTest(const char* name, void (*testFunc)());
+extern bool nearlyEqual(const double a, const double b);
 
 using namespace VecMatLib;
 
 void testMat2Addition() {
-    Mat2 a(), b(0, 0, 0, 0), c(1, 2, 3, 4), d(5, 6, 7, 8);
-    assert(c + d == Mat2(6,8,10,12));
+  Mat2 a(), b(0, 0, 0, 0), c(1, 2, 3, 4), d(5, 6, 7, 8);
+  assert(c + d == Mat2(6, 8, 10, 12));
 }
 
 void testMat2Multiplication() {
-    Mat2 a(), b(0, 0, 0, 0), c(1, 2, 3, 4), d(5, 6, 7, 8);
-    assert(c * d == Mat2(19,22,43,50));
+  Mat2 a(), b(0, 0, 0, 0), c(1, 2, 3, 4), d(5, 6, 7, 8);
+  assert(c * d == Mat2(19, 22, 43, 50));
 }
 
 void testMat2xVec2() {
-    Mat2 a(), b(0, 0, 0, 0), c(1, 2, 3, 4), d(5, 6, 7, 8);
-    vec2 v(1, 1);
-    assert(c * v == vec2(3,7));
+  Mat2 a(), b(0, 0, 0, 0), c(1, 2, 3, 4), d(5, 6, 7, 8);
+  Vec2 v(1, 1);
+  assert(c * v == Vec2(3, 7));
 }
 
 void testMat2Inverse() {
-    Mat2 a(), b(0, 0, 0, 0), c(1, 2, 3, 4), d(5, 6, 7, 8);
-    Mat2 I = c * c.inversed();
+  Mat2 c(1, 2, 3, 4);
+  Mat2 I = c * c.inversed();
 
-    assert(nearlyEqual(I.a, 1));
-    assert(nearlyEqual(I.b, 0));
-    assert(nearlyEqual(I.c, 0));
-    assert(nearlyEqual(I.d, 1));
+  assert(nearlyEqual(I.mat_[0][0], 1));
+  assert(nearlyEqual(I.mat_[0][1], 0));
+  assert(nearlyEqual(I.mat_[1][0], 0));
+  assert(nearlyEqual(I.mat_[1][1], 1));
 }
 
 void testMat2Transpose() {
-    Mat2 a(), b(0, 0, 0, 0), c(1, 2, 3, 4), d(5, 6, 7, 8);
-    Mat2 I = c.transposed();
+  Mat2 a(), b(0, 0, 0, 0), c(1, 2, 3, 4), d(5, 6, 7, 8);
+  Mat2 I = c.transposed();
 
-    assert(I == Mat2(1,3,2,4));
+  assert(I == Mat2(1, 3, 2, 4));
 }
 
 void runMat2Tests() {
-    runTest("Mat2 addition test", testMat2Addition);
-    runTest("mat2 multiplication test", testMat2Multiplication);
-    runTest("mat2 - vec2 multiplication test", testMat2xVec2);
-    runTest("mat2 inverse test", testMat2Inverse);
-    runTest("Mat2 transpose test", testMat2Transpose);
+  runTest("Mat2 addition test", testMat2Addition);
+  runTest("mat2 multiplication test", testMat2Multiplication);
+  runTest("mat2 - vec2 multiplication test", testMat2xVec2);
+  runTest("mat2 inverse test", testMat2Inverse);
+  runTest("Mat2 transpose test", testMat2Transpose);
+
+  std::cout << "Tests passed" << "\n\n";
 }
--- tests/vec3Test.cpp
+++ tests/vec3Test.cpp
@@ -1,64 +1,62 @@
 //
 // Created by chice on 3/24/2026.
 //
-#include<cmath>
-#include<cassert>
+#include <cassert>
+#include <cmath>
 #include <iostream>
 
-#include "../include/VecMatLib/vec3.h"
+#include "../include/VecMatLib/Vec3.h"
 #include "VecMatLib/Mat2.h"
 #include "VecMatLib/utils.h"
 using namespace VecMatLib;
 
 extern double EPS;
-vec3 a(1, 2, 3), b(4, 5, 6);
+extern void runTest(const char* name, void (*testFunc)());
+Vec3 a(1, 2, 3), b(4, 5, 6);
 
-extern bool nearlyEqual( double a, double b );
+extern bool nearlyEqual(double a, double b);
 
 void testVec3Addition() {
-    vec3 a(1, 2, 3), b(4, 5, 6);
-    vec3 result = a + b;
+  Vec3 a(1, 2, 3), b(4, 5, 6);
+  Vec3 result = a + b;
 
-    assert(result.x == 5 && result.y == 7 && result.z == 9);
+  assert(result.x == 5 && result.y == 7 && result.z == 9);
 }
 
 void testVec3Subtraction() {
-    vec3 result = a - b;
+  Vec3 result = a - b;
 
-    assert(result.x == -3 && result.y == -3 && result.z == -3);
+  assert(result.x == -3 && result.y == -3 && result.z == -3);
 }
 
 void testVec3Dot() {
-    double res = a.dot(b);
-    assert(res == 32);
+  double res = a.dot(b);
+  assert(res == 32);
 }
 
 void testVec3Cross() {
-    vec3 result = a.cross(b);
-    assert(result.x == -3 && result.y == 6 && result.z == -3);
+  Vec3 result = a.cross(b);
+  assert(result.x == -3 && result.y == 6 && result.z == -3);
 }
 
 void testVec3Dist() {
-    double dist = VecMatLib::distance(a, b);
-    assert(nearlyEqual(dist, std::sqrt(27)));
+  double dist = VecMatLib::distance(a, b);
+  assert(nearlyEqual(dist, std::sqrt(27)));
 }
 
-void runTest( const char* name, void (*testFunc)() ) {
-    std::cout << "Running " << name << "...";
-    testFunc();
-    std::cout << "OK\n";
-}
+
 
 void runVec3Tests() {
-    runTest("vec 3 addition test", testVec3Addition);
-    runTest("vec 3 subtraction test", testVec3Subtraction);
-    runTest("vec 3 dot test", testVec3Dot);
-    runTest("vec 3 cross test", testVec3Cross);
-    runTest("vec3 distance function test", testVec3Dist);
+  runTest("vec 3 addition test", testVec3Addition);
+  runTest("vec 3 subtraction test", testVec3Subtraction);
+  runTest("vec 3 dot test", testVec3Dot);
+  runTest("vec 3 cross test", testVec3Cross);
+  runTest("vec3 distance function test", testVec3Dist);
 
-    const vec3 big(1e9, 1e9, 1e9);
+  const Vec3 big(1e9, 1e9, 1e9);
 
-    assert(nearlyEqual(a.dot(b), b.dot(a)));
-    assert(big.magnitude() > 0);
-    assert(nearlyEqual(big.normal().magnitude(), 1));
+  assert(nearlyEqual(a.dot(b), b.dot(a)));
+  assert(big.magnitude() > 0);
+  assert(nearlyEqual(big.normal().magnitude(), 1));
+  std::cout << "Tests passed" << "\n\n";
 }
