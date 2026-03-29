46990b08493dbc56aaaa8f76360a575f5d69a717
chickenBicen <rds.jr24@gmail.com>
2026-03-29 15:33:40 -0500

    utils and such

--- include/VecMatLib/utils.h
+++ include/VecMatLib/utils.h
@@ -1,40 +1,37 @@
 #pragma once
-#include "vec2.h"
-#include "vec3.h"
-#include "mat2.h"
-#include <cmath>
 #include <algorithm>
+#include <cmath>
+
+#include "Vec2.h"
+#include "Vec3.h"
+#include "mat2.h"
 
 namespace VecMatLib {
-    vec2 lerp( const vec2& a, const vec2& b, double t );
-    vec3 lerp( const vec3& a, const vec3& b, double t );
+Vec2 lerp(const Vec2& a, const Vec2& b, double t);
+Vec3 lerp(const Vec3& a, const Vec3& b, double t);
 
-    double magnitude( const vec2& v );
+double magnitude(const Vec2& v);
 
-    Mat2 rotationMat2( double angle );
+Mat2 rotationMat2(double angle);
 
-    template<typename Vec>
-        requires requires( Vec v )
-        {
-            v.dot(v);
-            v.magnitude();
-        }
-    double angle( const Vec& a, const Vec& b ) {
-        double dot = a.dot(b);
-        double mag = b.magnitude() * a.magnitude();
+template <typename Vec>
+  requires requires(Vec v) {
+    v.dot(v);
+    v.magnitude();
+  }
+double angle(const Vec& a, const Vec& b) {
+  double dot = a.dot(b);
+  double mag = b.magnitude() * a.magnitude();
 
-        double val = dot / mag;
-        val = std::clamp(val, -1.0, 1.0);
-        return std::acos(val);
-    }
+  double val = dot / mag;
+  val = std::clamp(val, -1.0, 1.0);
+  return std::acos(val);
+}
 
-    template<typename Vec>
-        requires requires( Vec v )
-        {
-            v.magnitude();
-        }
-    double distance( const Vec& a, const Vec& b ) {
-        Vec ab = a - b;
-        return ab.magnitude();
-    }
-} // namespace vecmatlib
+template <typename Vec>
+  requires requires(Vec v) { v.magnitude(); }
+double distance(const Vec& a, const Vec& b) {
+  Vec ab = a - b;
+  return ab.magnitude();
+}
+}  // namespace VecMatLib
