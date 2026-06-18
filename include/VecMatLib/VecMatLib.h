de8c846e9527f83dfafeee1eff3323fed34b952c
chickenBicen <rds.jr24@gmail.com>
2026-06-17 23:55:27 -0500

    added an all import file

---
+++ include/VecMatLib/VecMatLib.h
@@ -0,0 +1,17 @@
+//
+// Created by chice on 6/17/2026.
+//
+
+#ifndef VECMATLIB_VECMATLIB_H
+#define VECMATLIB_VECMATLIB_H
+
+#include "Core/Mat2.h"
+#include "Core/Mat3.h"
+#include "Core/Quaternion.h"
+#include "Core/Vec2.h"
+#include "Core/Vec3.h"
+#include "Math/Transform2.h"
+#include "Math/Transform3.h"
+#include "Math/utils.h"
+
+#endif // VECMATLIB_VECMATLIB_H
